170a9d8b461774b05fe4c17a1619404aae40a59a
SafaMaqbool <s.maq.basha@gmail.com>
2026-07-16 00:11:34 +0500

    feat: enhance Footer component with dynamic social media icons and aria-labels; update BaseLayout for improved SEO metadata; add animation overrides in global styles

--- src/constants/footerLinks.ts
+++ src/constants/footerLinks.ts
@@ -20,8 +20,8 @@ export const footerLinks = {
 
   ],
   icons: [
-    { label: 'Twitter', href: 'https://twitter.com/groveradigital'
-     },
-    { label: 'LinkedIn', href: 'https://www.linkedin.com/company/grovera-digital/' },
+    { label: 'Twitter', href: 'https://twitter.com/groveradigital', icon: 'simple-icons:twitter' },
+    { label: 'Instagram', href: 'https://www.instagram.com/groveradigital/', icon: 'simple-icons:instagram' },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/company/grovera-digital/', icon: 'simple-icons:linkedin' },
   ],
 };
