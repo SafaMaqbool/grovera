1b3ff3d337df9746b7af914e85a811d28b57a33b
SafaMaqbool <s.maq.basha@gmail.com>
2026-07-17 20:33:28 +0500

    feat: add GitHub Actions workflow for deploying to GitHub Pages; update Astro config for site URL and base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://SafaMaqbool.github.io',
+  base: process.env.NODE_ENV === 'production' ? '/grovera' : '/',
   vite: {
     plugins: [tailwindcss()],
   },
